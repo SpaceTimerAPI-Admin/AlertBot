254979bc698c97f7dd53e6090979fd5178350c9d
SpaceTimerAPI-Admin <Anthony.mchughnh@gmail.com>
2025-07-12 15:28:45 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,1 +1,13 @@
-// Placeholder
+import 'dotenv/config';
+import { Client, GatewayIntentBits, Partials } from 'discord.js';
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+  partials: [Partials.Channel]
+});
+
+client.once('ready', () => {
+  console.log(`🤖 Bot ready as ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
